d6a241042ae695fbe72e8860b106a97886410020
andmehta <38476199+andmehta@users.noreply.github.com>
2020-02-04 22:54:38 -0600

    took the category determination and put it in its own function in order to test it properly

--- bmi.py
+++ bmi.py
@@ -6,6 +6,20 @@ def convert_to_kg(weight):
     return weight*.45
 
 
+def category_check(value):
+    if 0 < value < 18.5:
+        return "Underweight"
+
+    if 18.5 <= value < 25:
+        return "Normal"
+
+    if 25 <= value < 30:
+        return "Overweight"
+
+    if value >= 30:
+        return "Obese"
+
+
 # bmi is kg/(m)^2
 def bmi(height_f, height_i, weight):
     height_m = convert_to_meters(height_f, height_i)
@@ -13,16 +27,7 @@ def bmi(height_f, height_i, weight):
     kg = convert_to_kg(weight)
 
     bmi_value = round(kg/height_m**2, 1)
-    if 0 < bmi_value < 18.5:
-        category = "Underweight"
 
-    if 18.5 <= bmi_value < 25:
-        category = "Normal"
-
-    if 25 <= bmi_value < 30:
-        category = "Overweight"
-
-    if bmi_value >= 30:
-        category = "Obese"
+    category = category_check(bmi_value)
 
     return bmi_value, category
