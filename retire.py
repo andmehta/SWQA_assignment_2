9182ad868ecf13b710cc0f1006d40a3705f84176
andmehta <38476199+andmehta@users.noreply.github.com>
2020-02-04 19:38:36 -0600

    created retire function that determines age of retirement

--- retire.py
+++ retire.py
@@ -1,3 +1,13 @@
-def calculateEmployerMatching():
-    #stuff
+def retire(age, salary, pct_saved, goal):
+    total = 0
+    while total*1.35 < goal:
+        total += salary*pct_saved
+        age += 1
+
+    if age > 100.0:
+        return -1
+
+    else:
+        return age
+
 
